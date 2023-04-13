f02eee7598b174e07296fa1e9abc06b0022d1485
Daniel Devine <danieljdevine@mines.edu>
2023-04-13 16:46:12 -0600

    All tests passing for adjacent and sequential prefetchers

--- src/prefetchers.c
+++ src/prefetchers.c
@@ -31,14 +31,26 @@ uint32_t sequential_handle_mem_access(struct prefetcher *prefetcher,
                                       struct cache_system *cache_system, uint32_t address,
                                       bool is_miss)
 {
+    uint32_t N = *((uint32_t*)prefetcher->data);
     // TODO: Return the number of lines that were prefetched.
-    return 0;
+
+    int successes = 0;
+    for (uint32_t i=1; i<=N; i++) {
+        int success = cache_system_mem_access(cache_system, address + i*(cache_system->line_size), 'R', true);
+
+        if (success == 0) {
+            successes++;
+        }
+    }
+
+    return successes;
 }
 
 void sequential_cleanup(struct prefetcher *prefetcher)
 {
     // TODO cleanup any additional memory that you allocated in the
     // sequential_prefetcher_new function.
+    free(prefetcher->data);
 }
 
 struct prefetcher *sequential_prefetcher_new(uint32_t prefetch_amount)
@@ -49,6 +61,8 @@ struct prefetcher *sequential_prefetcher_new(uint32_t prefetch_amount)
 
     // TODO allocate any additional memory needed to store metadata here and
     // assign to sequential_prefetcher->data.
+    sequential_prefetcher->data = calloc(1, sizeof(uint32_t));
+    *((uint32_t*)sequential_prefetcher->data) = prefetch_amount;
 
     return sequential_prefetcher;
 }
@@ -60,9 +74,14 @@ uint32_t adjacent_handle_mem_access(struct prefetcher *prefetcher,
                                     bool is_miss)
 {
     // TODO perform the necessary prefetches for the adjacent strategy.
+    int success = cache_system_mem_access(cache_system, address + cache_system->line_size, 'R', true);
 
     // TODO: Return the number of lines that were prefetched.
-    return 0;
+    if (success == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 void adjacent_cleanup(struct prefetcher *prefetcher)
